5480f579175c19e69de996d224e516bb80e2bc80
Sabbella Bhagya Sai Anil Reddy <105065141+anilreddysbs@users.noreply.github.com>
2025-02-23 19:36:43 +0530

    Update settings.py

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['anil_to_do_list.pythonanywhere.com']
+ALLOWED_HOSTS = ['anilreddysbs.pythonanywhere.com']
 
 
 # Application definition
